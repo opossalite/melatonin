28d477647a02d9aea180e42b9fbb3808ac2aa6d0
opossalite <werbird10@gmail.com>
2025-06-18 21:40:24 -0700

    Learning checkpoint s3

--- music-svelte/src/routes/+page.svelte
+++ music-svelte/src/routes/+page.svelte
@@ -14,37 +14,7 @@
 
 <main>
     <p>Step: {formState.step}</p>
-    {#if formState.step === 0}
-        <div>
-            <label for="name">Your Name</label>
-            <input type="text" id="name" bind:value={formState.name}>
-        </div>
-        <button onclick={() => {
-            if (formState.name !== "") {
-                formState.step += 1;
-                formState.error = "";
-            } else {
-                formState.error = "Your name is empty. Please write your name."
-            }
-        }}>
-            Next
-        </button>
-    {:else if formState.step === 1}
-        <div>
-            <label for="bday">Your Name</label>
-            <input type="date" id="bday" bind:value={formState.birthday}>
-        </div>
-        <button onclick={() => {
-            if (formState.birthday !== "") {
-                formState.step += 1;
-                formState.error = "";
-            } else {
-                formState.error = "Your birthday is empty. Please write your name."
-            }
-        }}>
-            Next
-        </button>
-    {/if}
+    {@render formStep({question: "What's your name?", id: "name", type: "text"})}
     {#if formState.error}
         <p class="error">{formState.error}</p>
     {/if}
