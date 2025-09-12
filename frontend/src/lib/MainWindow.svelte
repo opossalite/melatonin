11622012ca4a1dedb2955dafba0d13a3da78c565
opossalite <werbird10@gmail.com>
2025-09-12 01:09:01 -0700

    Yeah I think I'm just gonna nuke the UI

--- frontend/src/lib/MainWindow.svelte
+++ frontend/src/lib/MainWindow.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
-    import LeftBar from "./LeftBar/LeftBar.svelte";
-    import Content from "./Content/Content.svelte";
+    import LeftBar from "./LeftBar.svelte";
+    import Content from "./Content.svelte";
     import { ProgramState } from "./structures.svelte";
     let {program_state}: {program_state: ProgramState} = $props();
 
--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
     import { browser } from "$app/environment";
-    import Header from "$lib/Header/Header.svelte";
-    import Footer from "$lib/Footer/Footer.svelte";
+    import Header from "$lib/Header.svelte";
+    import Footer from "$lib/Footer.svelte";
     import MainWindow from "$lib/MainWindow.svelte";
     import { onMount } from "svelte";
     import { ProgramState, type Album } from "$lib/structures.svelte";
